10819f629224444a16ecd047f2259397ba607f3f
DS9936 <ds963258@gmail.com>
2023-04-05 17:11:51 +0900

    포스트 상세 페이지에 부트스트랩 적용하기

--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,13 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
     # template_name = 'blog/post_list.html'
+
     # 'blog/post_list.html' : class 이름_list.html 은 내부적으로 정의가 되어있기때문에 생략 가능
+
 class PostDetail(DetailView):
     model = Post
-
+    # template_name = 'blog/post_detail.html'
+    
 def single_post_page(request,pk):
     post = Post.objects.get(pk=pk)
 
