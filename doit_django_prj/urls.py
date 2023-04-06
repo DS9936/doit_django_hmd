dcd7bd664fdc8feb357ad8bd40340b96a8e774a5
DS9936 <ds963258@gmail.com>
2023-04-06 10:04:09 +0900

    포스트 목록 페이지&상세 페이지에서 이미지 출력하기

--- doit_django_prj/urls.py
+++ doit_django_prj/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#미디어 파일 URL 지정
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls')),
 ]
+
+#미디어 파일 url 지정
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
